64d6eb6f71aaf8fd022c973dc25899563ad41d0e
JoseCerdaLora <josel.cerdal@unac.edu.co>
2025-05-27 18:32:45 -0500

    fix: corregir comentario funcionalidad 1

--- src/main/java/unac/edu/co/Main.java
+++ src/main/java/unac/edu/co/Main.java
@@ -2,6 +2,8 @@ package unac.edu.co;
 
 public class Main {
     public static void main(String[] args) {
+        // Comentario corregido de la funcionalidad 1
+
         System.out.println("Hello, gitflow!");
     }
 }
