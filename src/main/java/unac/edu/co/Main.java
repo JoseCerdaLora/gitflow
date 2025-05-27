8dd6e79c25c0a565d07c40c6f49ce65e56086398
JoseCerdaLora <josel.cerdal@unac.edu.co>
2025-05-27 18:30:33 -0500

    release: preparación para la versión 1.0.0

--- src/main/java/unac/edu/co/Main.java
+++ src/main/java/unac/edu/co/Main.java
@@ -2,6 +2,7 @@ package unac.edu.co;
 
 public class Main {
     public static void main(String[] args) {
+        // Preparando release 1.0.0
         System.out.println("Hello, gitflow!");
     }
 }
